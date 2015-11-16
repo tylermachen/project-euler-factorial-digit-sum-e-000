2be626c6ef7a1b3ccf6a7ed189d825fc1ac44d24
Tyler Machen <tylermachen@gmail.com>
2015-11-15 21:26:58 -0500

    done

--- lib/project_euler_factorial_digit_sum.rb
+++ lib/project_euler_factorial_digit_sum.rb
@@ -1,1 +1,12 @@
-# code your solution here
+def factorial(n)
+  return n if n <= 1
+  factorial(n - 1) * n
+end
+
+def sum_of_digits(n)
+  n.to_s.split('').map { |s| s.to_i }.reduce(:+)
+end
+
+def factorial_digit_sum(n)
+  sum_of_digits(factorial(n))
+end
